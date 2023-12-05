6394a2ec371c202e31a0a9b0e3a8198e3d4620e6
Mario-Vargas <104449353+MarioVargas-C@users.noreply.github.com>
2023-12-05 16:02:52 -0600

    update

--- src/main/java/org/utl/donGalleto/View/modules/cuentas/scripts.js
+++ src/main/java/org/utl/donGalleto/View/modules/cuentas/scripts.js
@@ -123,9 +123,65 @@ function render_grafico_inventario(nombres, valores){
     });
 }
 
+        // Función para generar datos aleatorios
+        function generarDatosAleatorios() {
+            const nombresGalletas = ['Chispas', 'Trigo', 'Menta', 'Oreo', 'Chocolate Blanco', 'Nuez', 'Arándanos', 'Polvorones', 'Canela', 'Vainilla'];
+            const cantidadMinima = 1;
+            const cantidadMaxima = 20;
+            const precioMinimo = 1.5;
+            const precioMaximo = 3.5;
+
+            const datos = [];
+
+            for (let i = 1; i <= 30; i++) {
+                const nombre = nombresGalletas[Math.floor(Math.random() * nombresGalletas.length)];
+                const cantidad = Math.floor(Math.random() * (cantidadMaxima - cantidadMinima + 1)) + cantidadMinima;
+                const precioUnitario = (Math.random() * (precioMaximo - precioMinimo) + precioMinimo).toFixed(2);
+                const total = (cantidad * precioUnitario).toFixed(2);
+                const fecha = obtenerFechaAleatoria();
+
+                datos.push({
+                    numero: i,
+                    nombre: nombre,
+                    cantidad: cantidad,
+                    precioUnitario: precioUnitario,
+                    total: total,
+                    fecha: fecha
+                });
+            }
+
+            return datos;
+        }
+
+        // Función para obtener una fecha aleatoria en formato YYYY-MM-DD
+        function obtenerFechaAleatoria() {
+            const fecha = new Date(2023, 0, 1 + Math.floor(Math.random() * 365));
+            const year = fecha.getFullYear();
+            const month = (fecha.getMonth() + 1).toString().padStart(2, '0');
+            const day = fecha.getDate().toString().padStart(2, '0');
+
+            return `${year}-${month}-${day}`;
+        }
+
+        // Función para llenar la tabla con datos aleatorios
+        function llenarTabla() {
+            const datosAleatorios = generarDatosAleatorios();
+            const tableBody = document.getElementById('tableBody_ventas_reporte');
+
+            datosAleatorios.forEach((dato) => {
+                const row = document.createElement('tr');
+                row.innerHTML = `<td>${dato.numero}</td>
+                                 <td>${dato.nombre}</td>
+                                 <td>${dato.cantidad}</td>
+                                 <td>${dato.precioUnitario}</td>
+                                 <td>${dato.total}</td>
+                                 <td>${dato.fecha}</td>`;
+                tableBody.appendChild(row);
+            });
+        }
 $(document).ready(function () {
     $('#mes').click();
-
+    llenarTabla();
     $.ajax({
         url : "http://localhost:8080/api/inventario/getAll",
         type : "GET",
