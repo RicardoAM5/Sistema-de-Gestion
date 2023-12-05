e512f7764c07d9c78e59a474f0868951df1f81be
Mario-Vargas <104449353+MarioVargas-C@users.noreply.github.com>
2023-12-05 14:32:51 -0600

    Update scripts.js

--- src/main/java/org/utl/donGalleto/View/modules/cuentas/scripts.js
+++ src/main/java/org/utl/donGalleto/View/modules/cuentas/scripts.js
@@ -22,8 +22,30 @@ new Chart(ctx, {
 });
 
 $(document).ready(function () {
+    var nombres = [];
+    var valores = [];
     $('.btn-check').on('change', function () {
-        var valorSeleccionado = $('input[name="filters"]:checked').val();
-        console.log('Valor seleccionado:', valorSeleccionado);
+        var filtro = $('input[name="filters"]:checked').val();
+        console.log('Valor seleccionado:', filtro);
+
+        $.ajax({
+            url : "http://localhost:8080/api/dashboard/ventasFecha",
+            type : "GET",
+            data:{"filtro": filtro},
+            success : function(resp) {
+                console.log(resp);
+                resp.forEach(function(item) {
+                    nombres.push(item.nombreGalleta);
+                    valores.push(item.cantidadVendida);
+                });
+            },
+            error: function(error) {
+                console.error(error);
+            }
+        });
     });
 });
+
+/* function render_grafico(nombres, valores){
+
+} */
