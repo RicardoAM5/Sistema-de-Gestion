88b03b81d7310c089c1b69d06ab2b713511c34df
Ricardo Alonso Medina <110549251+RicardoAM5@users.noreply.github.com>
2023-12-05 01:18:26 -0600

    Tablas actualizadas

--- database/tables.sql
+++ database/tables.sql
@@ -24,12 +24,25 @@ FOREIGN KEY (id_galleta) REFERENCES galleta(id_galleta)
 --Tabla venta--
 CREATE TABLE venta(
 id_venta SERIAL PRIMARY KEY NOT NULL,
-cantidad VARCHAR,
+cantidad NUMERIC,
 total VARCHAR,
-fecha timestamp, 
+fecha DATE, 
 id_galleta INT,
 FOREIGN KEY (id_galleta) REFERENCES galleta(id_galleta)
 );
+
+--Consulta que trae todas las ventas
+SELECT g.nombre AS nombre_galleta, v.cantidad AS cantidad_vendida , v.fecha as fecha 
+FROM venta v
+JOIN galleta g ON v.id_galleta = g.id_galleta;
+
+--Consulta que trae todas las ventas por fecha
+SELECT DATE(fecha) AS fecha, g.nombre AS nombre_galleta, SUM(v.cantidad) AS cantidad_vendida
+FROM venta v
+JOIN galleta g ON v.id_galleta = g.id_galleta
+GROUP BY DATE(fecha), g.nombre
+ORDER BY DATE(fecha), g.nombre;
+
 
 -- Tabla materia prima
 --NO USADA ACTUALMENTA
@@ -43,4 +56,39 @@ CREATE TABLE materia_prima(
 
 
 
+SELECT DATE(fecha) AS fecha, g.nombre AS nombre_galleta, SUM(v.cantidad) AS cantidad_vendida
+FROM venta v
+JOIN galleta g ON v.id_galleta = g.id_galleta
+GROUP BY DATE(fecha), g.nombre
+ORDER BY DATE(fecha), g.nombre;
+
+
+-- Filtra las ventas de hoy
+SELECT DATE(fecha) AS fecha, g.nombre AS nombre_galleta, SUM(v.cantidad) AS cantidad_vendida
+FROM venta v
+JOIN galleta g ON v.id_galleta = g.id_galleta
+WHERE DATE(fecha) = CURRENT_DATE 
+GROUP BY DATE(fecha), g.nombre
+ORDER BY DATE(fecha), g.nombre;
+
+
+--filtra las ventas de la semana
+SELECT DATE(fecha) AS fecha, 
+       g.nombre AS nombre_galleta, 
+       SUM(v.cantidad) AS cantidad_vendida
+FROM venta v
+JOIN galleta g ON v.id_galleta = g.id_galleta
+WHERE EXTRACT(WEEK FROM fecha) = EXTRACT(WEEK FROM CURRENT_DATE) -- Filtra las ventas de la semana actual
+GROUP BY DATE(fecha), g.nombre
+ORDER BY DATE(fecha), g.nombre;
+
+--Filtra las ventas del mes
+SELECT DATE(fecha) AS fecha, 
+       g.nombre AS nombre_galleta, 
+       SUM(v.cantidad) AS cantidad_vendida
+FROM venta v
+JOIN galleta g ON v.id_galleta = g.id_galleta
+WHERE EXTRACT(MONTH FROM fecha) = EXTRACT(MONTH FROM CURRENT_DATE) -- Filtra las ventas del mes actual
+GROUP BY DATE(fecha), g.nombre
+ORDER BY DATE(fecha), g.nombre;
 
