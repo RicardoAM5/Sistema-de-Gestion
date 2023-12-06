81f90581097afa9557921fd4c47b36dc5d34e6f0
Carlos R Bello <ramirezbelloci@gmail.com>
2023-12-05 18:09:03 -0600

    Modulos incompletos

--- src/main/java/org/utl/donGalleto/View/modules/galletas/galletas.js
+++ src/main/java/org/utl/donGalleto/View/modules/galletas/galletas.js
@@ -1,4 +1,23 @@
-function editarGalleta(data, ruta) {}
+function editarGalleta(
+  nombre,
+  precioUnitario,
+  precioKilo,
+  descripcion,
+  idGalleta,
+  imagenBase64
+) {
+  document.getElementById("agregarProductoModalLabel").value = "Editar";
+  document.getElementById("btn_agregar_galleta").value = "Editar";
+
+  document.getElementById("nombre").value = nombre;
+  document.getElementById("id").value = idGalleta;
+  document.getElementById("cantidad").value = cantidad;
+  document.getElementById("precio_unitario").value = precioUnitario;
+  document.getElementById("precio_kilo").value = precioKilo;
+  document.getElementById("descripcion").value = descripcion;
+  const img = document.getElementById("imagen-previa");
+  img.src = "data:image/png;base64," + imagenBase64;
+}
 
 function agregarGalleta() {
   const nombre = document.getElementById("nombre").value;
@@ -23,18 +42,18 @@ function agregarGalleta() {
 
     // Construir el objeto JSON
     const params = {
-      "nombre": nombre,
-      "cantidad": cantidad,
-      "precioUnitario": precioUnitario,
-      "precioKilo": precioKilo,
-      "descripcion": descripcion,
-      "imagenBase64": imagenBase64,
+      nombre: nombre,
+      cantidad: cantidad,
+      precioUnitario: precioUnitario,
+      precioKilo: precioKilo,
+      descripcion: descripcion,
+      imagenBase64: imagenBase64,
     };
     // Hacer la solicitud Fetch
     fetch("http://localhost:8080/api/galleta/save", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(params),
     })
@@ -42,11 +61,12 @@ function agregarGalleta() {
       .then((data) => {
         // Lógica para manejar la respuesta del servidor
         console.log("Respuesta del servidor:", data);
-        //alert("se agregó la galleta");
-        console.log(params)
+        //
         // // Cerrar el modal si es necesario
-        // const modal = new bootstrap.Modal(document.getElementById("addCookie"));
-        // modal.hide();
+        const modal = new bootstrap.Modal(document.getElementById("addCookie"));
+        modal.hide();
+        alert("se agregó la galleta");
+        location.reload();
       })
       .catch((error) => {
         // Lógica para manejar errores
@@ -55,7 +75,71 @@ function agregarGalleta() {
   };
 }
 
-function editarGalletaCommit() {}
+function editarGalletaCommit() {
+  const nombre = document.getElementById("nombre").value;
+  const id = document.getElementById("id").value;
+  const cantidad = document.getElementById("cantidad").value;
+  const precioUnitario = document.getElementById("precio_unitario").value;
+  const precioKilo = document.getElementById("precio_kilo").value;
+  const descripcion = document.getElementById("descripcion").value;
+
+  // Obtener el archivo de imagen y convertirlo a base64
+  const imagenInput = document.getElementById("imagen");
+  const imagenFile = imagenInput.files[0];
+  let imagenBase64 = "";
+  if (!imagenFile) {
+    const img = document.getElementById("imagen-previa");
+    imagenFile = img.src;
+    imagenBase64 = imagenFile.split(",")[1];
+  } else {
+    const reader = new FileReader();
+    reader.readAsDataURL(imagenFile);
+    reader.onloadend = function () {
+      imagenBase64 = reader.result.split(",")[1]; // Obtener el contenido en base64
+    };
+  }
+
+  // Construir el objeto JSON
+  const params = {
+    idGalleta: id,
+    nombre: nombre,
+    cantidad: cantidad,
+    precioUnitario: precioUnitario,
+    precioKilo: precioKilo,
+    descripcion: descripcion,
+    imagenBase64: imagenBase64,
+  };
+
+  // Hacer la solicitud Fetch
+  fetch("http://localhost:8080/api/galleta/update", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(params),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Respuesta del servidor:", data);
+      const modal = new bootstrap.Modal(document.getElementById("editCookie"));
+      modal.hide();
+      alert("Se actualizó la galleta");
+      location.reload();
+    })
+    .catch((error) => {
+      // Lógica para manejar errores
+      console.error("Error en la solicitud:", error);
+    });
+  document.getElementById("agregarProductoModalLabel").value = "Agregar";
+  document.getElementById("btn_agregar_galleta").value = "Agregar";
+
+  document.getElementById("nombre").value = "";
+  document.getElementById("id").value = "";
+  document.getElementById("cantidad").value = "";
+  document.getElementById("precio_unitario").value = "";
+  document.getElementById("precio_kilo").value = "";
+  document.getElementById("descripcion").value = "";
+}
 
 const eliminarGalleta = async (ruta, id) => {
   const swalWithBootstrapButtons = Swal.mixin({
@@ -92,6 +176,7 @@ const eliminarGalleta = async (ruta, id) => {
           title: "Galleta eliminada",
           icon: "success",
         });
+        alert("La página se recargará para mostrar cambios");
         location.reload();
       } else if (
         /* Read more about handling dismissals below */
---
+++ src/main/java/org/utl/donGalleto/View/modules/inventario/scripts.js
@@ -0,0 +1,75 @@
+import {
+    galletasApiUrl,
+    galletasUpdateApiUrl,
+    galletasDeleteApiUrl,
+    galletasInsertApiUrl,
+  } from "./modules/apiUrls.js";
+  
+
+  const listaGalletas = async () => {
+    try {
+      const response = await fetch(galletasApiUrl);
+      const data = await response.json();
+  console.log(data)
+      let content = "";
+      data.forEach((data, index) => {
+        content += `
+              <tr>
+              <td>${index + 1}</td>
+              <td>${data.nombre}</td>
+              <td>${data.cantidad}</td>
+              <td>
+                  <div class="d-flex">
+                  <button
+                      type="button"
+                      class="btn btn-success"
+                      
+            data-bs-toggle="modal"
+            data-bs-target="#addMaterial" 
+                      onclick="editarInventario('${data.idGalleta}','${
+          data.cantidad
+        }','${data.precioKilo}')"
+                      ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                      <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                    </svg>
+                      </button>
+                  
+                  </div>
+              </td>
+          </tr>
+          `;
+      });
+      tableBody_inventario.innerHTML = content;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  async function initialize() {
+    const galletas = await listaGalletas();
+    console.log(galletas);
+  }
+  
+  initialize();
+  
+  (() => {
+    "use strict";
+  
+    const forms = document.querySelectorAll(".valid-addcookie");
+  
+    Array.from(forms).forEach((form) => {
+      form.addEventListener(
+        "submit",
+        (event) => {
+          event.preventDefault();
+          event.stopPropagation();
+          if (form.checkValidity()) {
+            agregarGalleta();
+          }
+          form.classList.add("was-validated");
+        },
+        false
+      );
+    });
+  })();
